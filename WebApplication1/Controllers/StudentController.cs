36e6e91090062a847454fdfdbf86927595b83d12
twdeveloper <twdeveloper@outlook.com>
2019-08-16 16:44:10 +0800

    add student class

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            var dat = new List<Models.Student>();
+            dat.Add(new Models.Student { StuName = "David", Address = "台北", TEL = "0912345678" });
+            dat.Add(new Models.Student { StuName = "Eric", Address = "台北", TEL = "0912345412" });
+            dat.Add(new Models.Student { StuName = "Amy", Address = "台北", TEL = "0912356678" });
+
+            ViewData.Model = dat;
+            return View(dat);
+        }
+    }
+}
---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public string StuName { get; set; }
+        public string TEL { get; set; }
+        public string Address { get; set; }
+    }
+}
