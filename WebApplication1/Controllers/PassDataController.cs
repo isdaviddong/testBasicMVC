6b0e1411ddd1451a3d805faa83377d2b3f9f8c9c
twdeveloper <twdeveloper@outlook.com>
2019-08-16 15:53:53 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/PassDataController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class PassDataController : Controller
+    {
+        // GET: PassData
+        public ActionResult PassViewData()
+        {
+            ViewData["Name"] = "David";
+            ViewData["age"] = 18;
+            ViewData["TEL"] = "0912345678";
+
+            return View();
+
+        }
+
+        public ActionResult PassViewBag()
+        {
+            ViewBag.Name = "David";
+            ViewBag.age = 18;
+            ViewBag.TEL = "0912345678";
+            ViewBag.Address = "台北市";
+            ViewBag.XXXX = "台北市";
+
+            
+            return View();
+
+        }
+    }
+}
